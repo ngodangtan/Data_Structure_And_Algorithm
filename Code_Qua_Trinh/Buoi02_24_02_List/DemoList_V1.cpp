9640a676824e356174eec1520cc0f727f3faabc9
Tan Ngo Dang <ngodangtan1994@gmail.com>
2025-02-24 19:23:36 +0700

    init file buoi 02 qua trinh

---
+++ Code_Qua_Trinh/Buoi02_24_02_List/DemoList_V1.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Felix Ngo on 24/2/25.
+//
+
+#include <iostream>
+using namespace std;
+
+// Buoc 01: Tạo cấu trúc Node
+struct Node {
+  int data;
+  Node *pNext; // lưu trữ địa chỉ của Node phía sau
+};
+
+int main() {
+  return 0;
+}
