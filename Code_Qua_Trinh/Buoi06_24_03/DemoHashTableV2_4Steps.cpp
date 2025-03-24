76916f12f53d7399e2367aed4b988a172f4426ea
Tan Ngo Dang <ngodangtan1994@gmail.com>
2025-03-24 21:09:04 +0700

    update

--- Code_Qua_Trinh/Buoi06_24_03/DemoHashTableV2_4Steps.cpp
+++ Code_Qua_Trinh/Buoi06_24_03/DemoHashTableV2_4Steps.cpp
@@ -84,6 +84,20 @@ void add(HashTable& h, Node* p) {
     }
 }
 
+// Cau 1
+void initDataForHT(HashTable& h) {
+    add(h, 50);
+    add(h, initNode(73));
+    add(h, 35);
+    add(h, 36);
+    add(h, 64);
+    add(h, 28);
+    add(h, 90);
+    add(h, 21);
+    add(h, 53);
+    add(h, 23);
+}
+
 void printHashTable(HashTable h) {
     cout << "Hash Table. Size" << SIZE << endl;
     for (int i = 0; i < SIZE; i++) {
@@ -95,6 +109,39 @@ void printHashTable(HashTable h) {
     }
 }
 
+/* 3. Viết hàm tạo dữ liệu cho bảng băm từ mảng một chiều
+Input:
++ h: HashTable
++ arr: int[]
++ n: int
+Output:
++ h: HT&
++
+ */
+void initDataFromArray(HashTable& h, int a[], int n) {
+  for(int i = 0; i < n; i++) {
+    add(h, a[i]);
+  }
+}
+/* Viết hàm tìm giá trị có trong bảng băm
+Input:
++ h: HashTable
++ value: int
+Output:
++ bool: return
+ */
+bool timGiaTri(HashTable h, int value) {
+  // 1. Tìm BK có khả năng chứa giá trị
+  int viTri = hashFunc(value);
+  // 2. Tìm giá trị xem có trên BK dự tính
+  for(Node* p = h.buckets[viTri].pHead; p != NULL; p = p->pNext) {
+    if(p->data == value) {
+      return true;
+    }
+  }
+  return false;
+}
+
 int main() {
     HashTable h;
     initHashTable(h);
@@ -102,18 +149,20 @@ int main() {
     // Thử làm cấp phát động size dynamic v3
     HashTable h2;
     initHashTable(h2, 10);
+    cout << "Test: 1. Khởi tạo dữ liệu cho hashtable" << endl;
+    initDataForHT(h);
 
-    add(h, 50);
-    add(h, initNode(73));
-    add(h, 35);
-    add(h, 36);
-    add(h, 64);
-    add(h, 28);
-    add(h, 90);
-    add(h, 21);
-    add(h, 53);
-    add(h, 23);
-
+    cout << "\nTest: 2 In hash table:" << endl;
     printHashTable(h);
+    cout << "\nTest: 3. " << endl;
+    int a[] = {78,13,67,12,90};
+    int n = 5;
+    initDataFromArray(h, a, n);
+    printHashTable(h);
+    cout << "\nTest: 4. " << endl;
+    cout << "\nTest: 5. Tìm giá trị trong bảng băm" << endl;
+    cout << "\n + Tìm(67) = 1 = " << timGiaTri(h,67) << endl;
+    cout << "\n + Tìm(99) = 0 = " << timGiaTri(h,99) << endl;
+    cout << "\nTest: 6. " << endl;
     return 0;
 }
