00086c6750bf037646c600b7cb5a0ee6c74337f7
Tan Ngo Dang <ngodangtan1994@gmail.com>
2025-03-25 19:57:02 +0700

    update

--- Bai_tap/Buoi06/HashTable_SoThuc.cpp
+++ Bai_tap/Buoi06/HashTable_SoThuc.cpp
@@ -3,6 +3,41 @@
 // MSSV: 24210173@ms.uit.edu.vn
 //
 
+#include <iostream>
+using namespace std;
+
+const int SIZE = 5;
+
+// Bước 1: Định nghĩa cấu trúc Node
+struct Node {
+  float data;
+  Node* pNext;
+};
+
+// Bước 2: Cấp phát động, khởi tạo Node
+Node* initNode(float value)
+{
+  Node *p = new Node;
+  (*p).data = value;
+  p->pNext = NULL;
+  return p;
+}
+
+struct HashTable {
+  struct Bucket{
+    Node* pHead;
+    Node* pTail;
+  };
+  Bucket buckets[SIZE];
+};
+
+void initHashTable(HashTable& h) {
+  for (int i = 0; i < SIZE; i++) {
+    h.buckets[i].pHead = NULL;
+    h.buckets[i].pTail = NULL;
+  }
+}
+
 int main() {
   return 0;
 }
