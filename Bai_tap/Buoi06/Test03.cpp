c4402c3c4eae12b1d5e2cbdabc98ceb598bc1223
Tan Ngo Dang <ngodangtan1994@gmail.com>
2025-03-29 09:15:08 +0700

    update

--- Bai_tap/Buoi06/Test03.cpp
+++ Bai_tap/Buoi06/Test03.cpp
@@ -6,6 +6,37 @@
 #include <iostream>
 using namespace std;
 
+const int SIZE = 9;
+
+// Bước 1: Định nghĩa cấu trúc Node
+struct Node {
+  int data;
+  Node* pNext;
+};
+
+// Bước 2: Cấp phát động, khởi tạo Node
+Node* initNode(int value)
+{
+  Node *p = new Node;
+  (*p).data = value;
+  p->pNext = NULL;
+  return p;
+}
+
+struct HashTable {
+  struct Bucket{
+    Node* pHead;
+    Node* pTail;
+  };
+  Bucket buckets[SIZE];
+};
+
+int hashFunc(int  value) {
+  int valueInt = value;
+  return valueInt % SIZE;
+}
+
 int main() {
+
   return 0;
 }
