973d14dc4b416f602e2d419fd5f5c0c9f5759a61
Tan Ngo Dang <ngodangtan1994@gmail.com>
2025-04-14 19:38:09 +0700

    update

--- Code_Qua_Trinh/Buoi08_14_04/DemoSearch.cpp
+++ Code_Qua_Trinh/Buoi08_14_04/DemoSearch.cpp
@@ -13,13 +13,44 @@ void printArr(int* a, int n) {
   }
 }
 
-bool linearSearch(int* a, int n) {
+bool linearSearch(int* a, int n, int value) {
+  for(int i = 0; i < n; i++) {
+    if(*(a+i) == value)
+      return a+i;
+  }
+  return NULL;
+}
 
+bool linearSearch2(int a[], int n, int value) {
+  for(int i = 0; i < n; i++) {
+    if(a[i] == value)
+      return true;
+  }
+  return false;
+}
+
+bool binarySearch(int a[], int n , int value) {
+  int L = 0, R = n -1, M;
+  while(L <= R) {
+    M = (L + R) / 2;
+    if(a[M] == value) {
+      return true;
+    }
+
+    if(value < a[M]) {
+      R = M - 1;
+    } else if(value > a[M]) {
+      L = M+ 1;
+    }
+  }
 }
 
 int main() {
   int a[] = {10, 9, 5 , 6, 7, 8, 4};
   int n = 6;
   printArr(a, n);
+  cout << "Test: Linear Search" << endl;
+  cout << "Ket qua tim 8(.): " << linearSearch(a, n, 8) << endl;
+  cout << "Ket qua tim 4(.: " << linearSearch(a, n, 4) << endl;
   return 0;
 }
