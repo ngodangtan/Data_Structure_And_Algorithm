ee659e92103da8a3f2eff52177e70352322424e0
Tan Ngo Dang <ngodangtan1994@gmail.com>
2025-04-14 21:53:29 +0700

    update

--- Code_Qua_Trinh/Buoi08_14_04/DemoSearch.cpp
+++ Code_Qua_Trinh/Buoi08_14_04/DemoSearch.cpp
@@ -43,6 +43,24 @@ bool binarySearch(int a[], int n , int value) {
       L = M+ 1;
     }
   }
+  return false;
+}
+
+int* binarySearch2(int *a, int n , int value) {
+  int L = 0, R = n -1, M;
+  while(L <= R) {
+    M = (L + R) / 2;
+    if(*(a + M) == value) {
+      return a + M;
+    }
+
+    if(value < *(a + M)) {
+      R = M - 1;
+    } else if(value > *(a + M)) {
+      L = M+ 1;
+    }
+  }
+  return NULL;
 }
 
 int main() {
@@ -52,5 +70,12 @@ int main() {
   cout << "Test: Linear Search" << endl;
   cout << "Ket qua tim 8(.): " << linearSearch(a, n, 8) << endl;
   cout << "Ket qua tim 4(.: " << linearSearch(a, n, 4) << endl;
+
+  int b[] = {10, 15, 36 , 58, 62, 97};
+  int m = 6;
+  printArr(b, m);
+  cout << "Test: Binary Search" << endl;
+  cout << "Ket qua tim 8(.): " << binarySearch(b, m, 36) << endl;
+  cout << "Ket qua tim 4(.: " << binarySearch(b, m, 40) << endl;
   return 0;
 }
